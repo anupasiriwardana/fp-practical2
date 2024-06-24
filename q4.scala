2cc772140aa66cd4fb277490761cf9f9c66ac99c
Anupa Siriwardana <anupasiriwardana123@gmail.com>
2024-06-24 22:44:21 +0530

    updated q4 answer-found max profit

--- q4.scala
+++ q4.scala
@@ -16,8 +16,10 @@ object showProfit{
     }
 
     def main(args:Array[String]):Unit = {
+        println("Profit when ticket price is 10 : "+profit(10))
         println("Profit when ticket price is 15 : "+profit(15))
         println("Profit when ticket price is 20 : "+profit(20))
-        println("Profit when ticket price is 10 : "+profit(10))
+        println("Profit when ticket price is 25 : "+profit(25))
+        println("Profit when ticket price is 30 : "+profit(30))
     }
 }
